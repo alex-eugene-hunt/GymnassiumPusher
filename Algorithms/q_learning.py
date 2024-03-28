102e3d6ffa53f58ea7f2ada14c09ff1c5e01dc57
alex-eugene-hunt <alex.eugene.hunt@gmail.com>
2024-03-28 08:36:00 -0500

    Added Makefile and code to start environment in q-learning.

--- Algorithms/q_learning.py
+++ Algorithms/q_learning.py
@@ -1,3 +1,18 @@
 def run_q_learning():
-    print("Hello from q_learning!")
+
+    import gymnasium as gym
+    from gymnasium import utils
+    from gymnasium.envs.mujoco.pusher_v4 import PusherEnv
+
+    env = PusherEnv(render_mode='human')
+    observation = env.reset()
+
+    for _ in range(3000):
+        action = env.action_space.sample()  # Randomly sample an action
+        observation, reward, done, info, _ = env.step(action)
+
+
+        if done:
+            observation = env.reset()
+
     return
